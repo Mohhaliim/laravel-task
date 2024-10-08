c3e2fdcbf6c13fb48d5c74b4ab9f2f80e63cba2f
Mohhaliim <mohamed.abd-elhalim@hotmail.com>
2024-10-08 13:42:08 +0300

    removed an extra comment

--- app/Http/Controllers/ListMailController.php
+++ app/Http/Controllers/ListMailController.php
@@ -8,9 +8,6 @@ class ListMailController extends Controller
 {
     public function index(): \Illuminate\View\View
     {
-        /* $emails = EmailSubscription::pluck('email','email')->toArray(); */
-
-
         return view('sadmin.list_mail.index');
     }
 }
